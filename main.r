9e2effccd759c132420e2f6363c6ea3eb9272a02
liugf <liugf@vip.qq.com>
2014-11-27 09:17:27 +0800

    add bias

--- main.r
+++ main.r
@@ -1,16 +1,16 @@
 source(file="tools.r",encoding="UTF-8")
 source(file="strategy1.r",encoding="UTF-8")
 
-symbol<-"01668.HK"
+symbol<-"03983.HK"
 stock<-read(symbol)['2012-01/2014-10']
 
 tdata<-trade_signal(stock)
 result<-trade(tdata,100000)
 
 g<-draw_close(stock)
-# g<-draw_volume(g, stock)
+g<-draw_volume(g, stock)
 g<-draw_macd(g, stock)
 g<-draw_bias(g, stock)
-# g<-draw_cash(g, result)
+g<-draw_cash(g, result)
 g<-draw_range(g, result, stock)
 g
