11f2a7e0c9df12fd4f7930d9c09409f88f46f644
liugf <liugf@vip.qq.com>
2015-05-01 23:42:27 +0800

    add message function

---
+++ oanda/message.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send(content = ''):
+    fromaddr = 'gavinlau.robot@gmail.com'
+    toaddr  = 'trigger@recipe.ifttt.com'
+    msg = MIMEText('')
+    msg['Subject'] = '%s #oanda' % (content,)
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    username = 'gavinlau.robot'
+    password = 'glave1234567890'
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(fromaddr, toaddr, msg.as_string())
+    server.quit()
+    return True
